05b6076844d2e945c5d6c03531e6a6a12e4d0285
Asadullah Hil Galib <86350811+codergalib20@users.noreply.github.com>
2022-05-23 22:35:43 +0600

    Commited

---
+++ components/customs/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container container">
+          <input type="checkbox" name="" id="" />
+          <div className="hamburger-lines">
+            <span className="line line1"></span>
+            <span className="line line2"></span>
+            <span className="line line3"></span>
+          </div>
+          <ul className="menu-items">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Category</a>
+            </li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Testimonial</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+          <h1 className="logo">Navbar</h1>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ components/customs/PreLoader.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const PreLoader = () => {
+  return (
+    <div className="css-pre-loader-wraper-main-parent-box">
+      <div className="css-pre-loader-wraper">
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+        <div className="cssload-circle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
